8e5b96e51e41b8ddc4f3b0042e266d58a197f33a
coder <bizdb@kakao.com>
2022-11-23 17:52:06 +0900

    11.23(two)
    ..

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="ko">
       <Head>
         <link
           href="https://fonts.googleapis.com/css2?family=Crete+Round&family=Work+Sans:wght@500;600&display=swap"
